2d4950c7f40b1bf344d31b93630bfaed7ea1d961
devofs2 <dev@ofs.bj>
2024-03-28 18:20:28 +0100

    get old version of erpnext

--- erpnext/stock/doctype/suivi_des_activations_et_des_fiches_de_souscription/suivi_des_activations_et_des_fiches_de_souscription.js
+++ erpnext/stock/doctype/suivi_des_activations_et_des_fiches_de_souscription/suivi_des_activations_et_des_fiches_de_souscription.js
@@ -7,39 +7,6 @@ frappe.ui.form.on("Suivi des activations et des fiches de souscription", {
 	},
 
     onload: function(frm) {
-        // Charger les données depuis le Doctype "Depot vente cartes"
-        /* var static_data = [
-            {
-                date_du_dépôt: '2023-01-01',
-                nombre_de_cartes: 10,
-                référence_du_bc: 'BC001',
-                distributeur: 'Distributeur 1',
-                commercial: 'Commercial 1'
-            },
-            {
-                date_du_dépôt: '2023-01-02',
-                nombre_de_cartes: 15,
-                référence_du_bc: 'BC002',
-                distributeur: 'Distributeur 2',
-                commercial: 'Commercial 2'
-            },
-            // Ajoutez d'autres lignes statiques selon vos besoins
-        ];
-    
-        // Parcourir les données statiques et les ajouter au tableau
-        if (static_data) {
-            $.each(static_data, function(index, item) {
-                var row = frm.add_child('cartes_depot_vente');
-                row.date_depot = item.date_du_dépôt;
-                row.nombre_des_cartes = item.nombre_de_cartes;
-                row.reference_du_bc = item.référence_du_bc;
-                row.distributeur = item.distributeur;
-                row.commercial = item.commercial;
-                // Ajoutez d'autres champs de votre Doctype principal selon vos besoins
-            });
-            frm.refresh_field('cartes_depot_vente');
-        } */
-
         frappe.call({
             method: 'frappe.client.get_list',
             id_cartes =j
